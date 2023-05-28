8117c8db4bf8b87a121d8091807e7d61df087c71
KARAN RANA <103360966+karananu@users.noreply.github.com>
2023-05-29 00:02:47 +0530

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -78,7 +78,7 @@ const Portfolio = () => {
   return (
     <div name="portfolio" className="bg-gradient-to-b from-black to-gray-800 w-full text-white h-full">
         <div className='max-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full '>
-            <div className='pt-8'>
+            <div className='pt-12'>
                 <p className="text-4xl font-bold inline border-b-4
                 border-gray-500 ">PORTFOLIO</p>
                 <p className='py-6'>
